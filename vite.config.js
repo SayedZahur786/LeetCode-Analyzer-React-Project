31924d452d0ceb3c321be03df71b0883ee339301
Mohd Jahur <syedzahurulhasan04@gmail.com>
2025-04-23 00:18:38 +0530

    Added base to Vite.config.js for deployment
    Added Base in Config File

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/LeetCode-Analyzer",
+  base: process.env.VITE_BASE_PATH || "/LeetCode-Analyzer-React-Project",
 })
